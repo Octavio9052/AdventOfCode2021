b82603c1e46a537b5dcf178462b32ba295be4b83
Octavio Armenta <armenta.octavio@outlook.com>
2021-12-01 12:04:39 -0800

    Add ugly main menu for C#

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,14 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AdventOfCsharp
 {
-    class Program
+    static class Program
     {
+        private static readonly Dictionary<int, IProcessDayRiddle> RiddleSolvers = new()
+        {
+            { 1, new SonarSweep() }
+        };
+
+        private const string Welcome =
+            @"                          
+                          ADVENT OF CODE 2021
+                          ";
+
+        private const string InvalidDay = "Invalid day riddle selected; starts at 1, ends at ";
+        private const string SelectDayMessage = "Please select a day from 1 to ";
+
         static void Main(string[] args)
         {
-            IProcessDayRiddle _dayOne = new SonarSweep();
-            _dayOne.ExecuteSolutions();
-            _dayOne.PrintSolutions();
+            Console.WriteLine(Welcome);
+            Console.WriteLine(SelectDayMessage + RiddleSolvers.Count);
+            var day = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(day) || !int.TryParse(day, out var dayNumber) || int.Parse(day) <= 0 || int.Parse(day) > RiddleSolvers.Count)
+            {
+                Console.WriteLine(InvalidDay + RiddleSolvers.Count);
+                Console.WriteLine(InvalidDay + RiddleSolvers.Count);
+                Main(args);
+            }
+            else
+            {
+                var daySolver = RiddleSolvers[int.Parse(day)];
+                daySolver.ExecuteSolutions();
+                daySolver.PrintSolutions();
+            }
         }
+
     }
 }
